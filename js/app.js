9b61900b7d01f5387217a94a7aa63391d4fdec30
Ethan Bensman <ebenz99@gmail.com>
2019-10-02 18:43:16 -0400

    window vs document issue

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
 function myFunction() {
-  window.open("https://www.w3schools.com");
+  window.open("https://www.when2meet.com/?8207981-daEOS");
+}
+
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
 myFunction();
+var i = 1;
+var moreDates = true;
+start = '//*[@id="GroupGrid"]/div[3]/div[';
+
+while (moreDates == true){
+	try {
+		var el = start.concat(i.toString(10),']');
+		console.log(el);
+		var date = getElementByXpath(el);
+		console.log(date);
+		if (date == null){
+			moreDates = false;
+		}
+		i+=1;
+	}
+	catch {
+		moreDates = false;
+	}
+	if (i > 10){
+		break;
+	}
+}
+
+//*[@id="GroupGrid"]/div[3]/div[1]
+//*[@id="GroupGrid"]/div[3]/div[1]
